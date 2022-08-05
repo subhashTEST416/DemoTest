5eef4c88163817a8d76c8d38cfff52bb6766c58a
Subhash <Subhash@DESKTOP-GD3J9I1>
2022-08-05 16:23:31 +0530

    test

---
+++ DemoTest/src/VimBar.java
@@ -0,0 +1,11 @@
+
+public class VimBar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("kitchen king");
+	}
+
+}
---
+++ DemoTest/src/VimBar2.java
@@ -0,0 +1,11 @@
+
+public class VimBar2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("kitchen king");
+	}
+
+}
